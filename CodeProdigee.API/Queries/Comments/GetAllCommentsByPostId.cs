983243e8e43e2776ec562f3ec2ddb536cec9d568
Joe Izang <josephizang@gmail.com>
2021-12-24 01:53:30 +0100

    Add handler for commentsbypostid

---
+++ CodeProdigee.API/Queries/Comments/GetAllCommentsByPostId.cs
@@ -0,0 +1,20 @@
+﻿using CodeProdigee.API.Dtos;
+using CodeProdigee.API.Dtos.Comments;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeProdigee.API.Queries.Comments
+{
+    public class GetAllCommentsByPostId : IRequest<CollectionResponse<CommentDto>>
+    {
+    }
+
+    public class GetAllCommentsByPostIdHandler : IRequestHandler<GetAllCommentsByPostId, CollectionResponse<CommentDto>>
+    {
+        public Task<CollectionResponse<CommentDto>> Handle(GetAllCommentsByPostId request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
